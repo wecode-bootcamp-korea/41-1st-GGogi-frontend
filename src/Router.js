10a538c732f604adef2b5e684b15cb8d0e176c44
윤지수 <songaji@yunjisuui-MacBookAir.local>
2023-01-02 18:37:58 +0900

    Modify : Login, SignUp, Footer Component 추가

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
@@ -9,7 +12,10 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
