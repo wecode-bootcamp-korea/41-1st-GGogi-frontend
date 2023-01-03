977177fa089f25ca9727dbdca90a1462be7160f8
Hailey_ys <whassup_man@naver.com>
2023-01-03 13:56:33 +0900

    Add : ShoppingModal

--- src/components/ShoppingModal/ShoppingModal.js
+++ src/components/ShoppingModal/ShoppingModal.js
@@ -3,25 +3,25 @@ import './ShoppingModal.scss';
 
 const ShoppingModal = () => {
   return (
-    <div className="ShoppingModal">
-      <div className="ModalTop">
-        <div>[버블몽키]캔디밤 솰라사ㅗㄹ라</div>
-        <div>
+    <div className="shoppingModal">
+      <div className="shoppingModalTop">
+        <div className="shoppingModalName">[예찬원] 사골 미역국</div>
+        <div className="shoppingModalCount">
           <div>2900원</div>
           <div>- 수량 +</div>
         </div>
       </div>
 
-      <div className="ModalMiddle">
-        <div>
-          <div>합계</div>
+      <div className="shoppingModalMiddle">
+        <div className="shoppingModalPrice">
+          <span>합계</span>
           <div>6900원</div>
         </div>
-        <div>로그인 후, 회원가입 적립혜택 제공</div>
+        <span>로그인 후, 회원가입 적립혜택 제공</span>
       </div>
-      <div className="ModalBottom">
-        <div>취소</div>
-        <div>장바구니 담기</div>
+      <div className="shoppingModalBottom">
+        <button>취소</button>
+        <button>장바구니 담기</button>
       </div>
     </div>
   );
