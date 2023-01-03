2813898f801588d5cae9165e83b89409cef273a3
Hailey_ys <whassup_man@naver.com>
2023-01-03 21:59:44 +0900

    Add : modal CSS

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AlertModal from '../../components/AlertModal/AlertModal';
 import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div>test</div>
+      <AlertModal />
+    </div>
+  );
 };
 
 export default Main;
