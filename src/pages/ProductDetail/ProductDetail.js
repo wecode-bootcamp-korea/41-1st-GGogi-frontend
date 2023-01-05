8d6c3f1a61c03b19032096efe2a5ce0b649d6324
Hailey_ys <whassup_man@naver.com>
2023-01-05 15:31:15 +0900

    Add productDetailInfo

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './ProductDetail.scss';
 
 const ProductDetail = () => {
   return (
-    <div>ProductDetail</div>
-  )
-}
+    <div className="productDetail">
+      <div className="productDetailImg">사진</div>
+      <section className="productDetailInfo">
+        <div className="productDetailName">
+          <div className="productDetailNameTop">샛별배송</div>
+          <div className="productDetailNameMiddle">
+            <div className="productDetailNameMiddleL">
+              [kF365] 양념 소불고기 1kg (냉장)
+            </div>
+            <div className="productDetailNameMiddleR">공유버튼</div>
+          </div>
+        </div>
+        <div className="productDetailPrice">18,990원</div>
+        <div className="productDetailInfo">상세정보표</div>
+        <div className="productDetailCard">상품추가카드</div>
+      </section>
+    </div>
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
---
+++ src/pages/ProductDetail/ProductDetailInfo/ProductDetailInfo.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const productDetailInfo = () => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th scope="col">배송</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">판매자</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">포장타입</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">판매타입</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">판매단위</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">중량/용량</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">원산지</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">알레르기정보</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">유통기한(또는 소비기한)정보</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">축산물이력정보</th>
+          <td />
+        </tr>
+        <tr>
+          <th scope="col">안내사항</th>
+          <td />
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default productDetailInfo;
