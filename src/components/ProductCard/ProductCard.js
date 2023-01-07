72f6528d1a3f6987413115be703ec6c9e635b5a6
Hailey_ys <whassup_man@naver.com>
2023-01-07 20:34:55 +0900

    Add productlist에서 detail page 연결

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import shoppingCart from '../../assets/images/shoppingCart.svg';
 import './ProductCard.scss';
 
 const ProductCard = ({ product }) => {
+  const navigate = useNavigate();
+  const showProduct = (id) => {
+    navigate(`/productdetail/${id}`);
+  };
   const { img, title, price } = product;
   return (
     <div className="productCard">
-      <img className="productImg" src={img} alt="productImg" />
+      <img
+        className="productImg"
+        src={img}
+        alt="productImg"
+        onClick={() => showProduct(product.id)}
+      />
       <button className="shoppingCartBtn">
         <img className="shoppingCart" src={shoppingCart} alt="shoppingCart" />
       </button>
