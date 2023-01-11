59b777d148312853b799b92e013260b6f6f88862
Ha-Pyeongan <96smilepeace@gmail.com>
2023-01-11 17:04:41 +0900

    Update : 리뷰 반영

--- src/pages/MyPage/components/MypageUserInfo.jsx
+++ src/pages/MyPage/components/MypageUserInfo.jsx
@@ -14,10 +14,6 @@ const MypageUserInfo = () => {
     setUserNewPwd({ ...userNewPwd, [name]: value });
   };
 
-  const { oldPassword, newPassword, againPassword } = userNewPwd;
-
-  const { email, name, phone } = userAddressData;
-
   const changePsw = (e) => {
     e.preventDefault();
     if (newPassword === againPassword) {
@@ -35,6 +31,10 @@ const MypageUserInfo = () => {
     }
   };
 
+  const { oldPassword, newPassword, againPassword } = userNewPwd;
+
+  const { email, name, phone } = userAddressData;
+
   useEffect(() => {
     fetch(`http://10.58.52.62:3000/users/profile`, {
       method: 'GET',
@@ -46,6 +46,54 @@ const MypageUserInfo = () => {
       .then((response) => response.json())
       .then((userAddress) => setUserAddressData(userAddress.data[0]));
   }, []);
+
+  const USERMODIFY = [
+    {
+      id: 1,
+      name: 'email',
+      title: '이메일',
+      type: 'email',
+      placeholder: email,
+    },
+    {
+      id: 2,
+      name: 'oldPassword',
+      title: '현재 비밀번호',
+      type: 'password',
+      placeholder: '현재 사용중인 비밀번호를 입력해주세요',
+    },
+    {
+      id: 3,
+      name: 'newPassword',
+      title: '새 비밀번호',
+      type: 'password',
+      placeholder: '변경하실 비밀번호를 입력해주세요',
+    },
+    {
+      id: 4,
+      name: 'againPassword',
+      title: '새 비밀번호 확인',
+      type: 'password',
+      placeholder: '비밀번호를 한 번 더 입력해주세요',
+    },
+    {
+      id: 5,
+      name: 'named',
+      title: '이름',
+      type: 'text',
+      placeholder: name,
+    },
+    {
+      id: 6,
+      name: 'phone',
+      title: '휴대폰',
+      type: 'tel',
+      placeholder: phone,
+    },
+  ];
+
+  console.log(userNewPwd);
+
   return (
     <div className="mypageUserModify">
       <div className="userModifyHaader">
@@ -60,12 +108,24 @@ const MypageUserInfo = () => {
             <form className="usersInfoModify" key={id}>
               <div className="userInfoModifytitle">{title}</div>
               <div className="userInputTextFiled">
-                <input
-                  className="userInputModify"
-                  name={name}
-                  type={type}
-                  placeholder={placeholder}
-                />
+                {id >= 2 && id <= 4 ? (
+                  <input
+                    className="userInputModify"
+                    name={name}
+                    type={type}
+                    placeholder={placeholder}
+                    autoComplete="on"
+                    value={userNewPwd[name]}
+                    onChange={passwordValue}
+                  />
+                ) : (
+                  <input
+                    className="userInputModify"
+                    name={name}
+                    type={type}
+                    placeholder={placeholder}
+                  />
+                )}
               </div>
             </form>
           );
@@ -81,110 +141,3 @@ const MypageUserInfo = () => {
   );
 };
 export default MypageUserInfo;
-
-const USERMODIFY = [
-  {
-    id: 1,
-    name: 'email',
-    title: '이메일',
-    type: 'email',
-    placeholder: email,
-  },
-  {
-    id: 2,
-    name: 'oldPassword',
-    title: '현재 비밀번호',
-    type: 'password',
-    placeholder: '현재 사용중인 비밀번호를 입력해주세요',
-  },
-  {
-    id: 3,
-    name: 'newPassword',
-    title: '새 비밀번호',
-    type: 'password',
-    placeholder: '변경하실 비밀번호를 입력해주세요',
-  },
-  {
-    id: 4,
-    name: 'againPassword',
-    title: '새 비밀번호 확인',
-    type: 'password',
-    placeholder: '비밀번호를 한 번 더 입력해주세요',
-  },
-  {
-    id: 5,
-    name: 'named',
-    title: '이름',
-    type: 'text',
-    placeholder: name,
-  },
-  {
-    id: 6,
-    name: 'phone',
-    title: '휴대폰',
-    type: 'tel',
-    placeholder: phone,
-  },
-];
-
-{
-  /* <form className="usersInfoModify">
-<div className="userInfoModifytitle">이메일</div>
-<div className="userInputTextFiled">
-  <input
-    className="userInputModify"
-    name="email"
-    type="email"
-    placeholder={email}
-  />
-</div>
-</form>
-<form className="usersInfoModify">
-<div className="userInfoModifytitle">현재 비밀번호</div>
-<div className="userInputTextFiled">
-  <input
-    className="userInputModify"
-    name="oldPassword"
-    type="password"
-    value={oldPassword}
-    onChange={passwordValue}
-    placeholder="현재 사용중인 비밀번호를 입력해주세요"
-    autoComplete="on"
-  />
-</div>
-</form>
-<form className="usersInfoModify">
-<div className="userInfoModifytitle">새 비밀번호</div>
-<div className="userInputTextFiled">
-  <input
-    className="userInputModify"
-    name="newPassword"
-    type="password"
-    value={newPassword}
-    onChange={passwordValue}
-    placeholder="변경하실 비밀번호를 입력해주세요"
-    autoComplete="on"
-  />
-</div>
-</form>
-<form className="usersInfoModify">
-<div className="userInfoModifytitle">새 비밀번호 확인</div>
-<div className="userInputTextFiled">
-  <input
-    className="userInputModify"
-    name="againPassword"
-    type="password"
-    value={againPassword}
-    onChange={passwordValue}
-    placeholder="비밀번호를 한 번 더 입력해주세요"
-    autoComplete="on"
-  />
-</div>
-</form>
-{newPassword !== againPassword && (
-<div className="againPwd">* 비밀번호를 확인 해주세요.*</div>
-)}
-<form className="usersInfoModify">
-<div className="userInfoModifytitle">이름</div>
-<div className="userInputTextFiled"> */
-}
